62ea9dac6751fcc0dadb39758ad56aef54f86e5b
Ben Doherty <bendohmv@gmail.com>
2014-03-13 02:43:59 -0400

    Lots of refactoring, add progress bar, fix various bugs in keeping data in sync, now load answer sheet sequentially to avoid weird race condition bugs

--- js/w3f-survey.js
+++ js/w3f-survey.js
@@ -6,155 +6,45 @@ Array.prototype.range = function(n) {
 	return Array.apply(null, Array(n)).map(function (_, i) {return i;});
 }
 
-angular.module('W3FODB', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute' ])
+angular.module('W3FWIS', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute', 'ngSanitize' ])
+	// Setup route. There's only one route, and it's /<answerSheetKey>
 	.config([ '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 		$routeProvider.when('/:answerKey?', {
-			controller: 'W3FSurvey',
+			controller: 'W3FSurveyController',
 			templateUrl: 'tpl/survey.html'
 		});
 
 		$locationProvider.html5Mode(true);
 	} ])
-	.controller('W3FSurvey', [ 'spreadsheets', '$scope', '$rootScope', '$routeParams', '$interval', function(gs, $scope, $rootScope, $routeParams, $interval) {	
-		if(!$routeParams.answerKey) {
-			return;
-		}
 
+	// Create "country" filter
+	.filter('country', [ '$rootScope', function( $rootScope ) {
+		return function(input) {
+			return input.replace('[country]', $rootScope.country);
+		}
+	} ])
+
+	// Create "markdown" filter
+	.filter('markdown', function( $rootScope ) {
+		return function(input) {
+			return markdown.toHTML(input);
+		}
+	})
+
+	// Top-level controller
+	.controller('W3FSurveyController', [ 'spreadsheets', '$scope', '$rootScope', '$q', '$cookies', '$routeParams', '$interval', function(gs, $scope, $rootScope, $q, $cookies, $routeParams, $interval) {	
+		var masterKey = '0AokPSYs1p9vhdEdjeUluaThWc2RqQnI0c21oN1FaYUE';
 		var answerKey = $routeParams.answerKey;
 		var answerSheet;
 
-		// Try to get answer sheet once an accessToken is found
-		$rootScope.$watch('accessToken', function(accessToken) {
-			if(!accessToken) {
-				return;
-			}
-
-			// Try to get answer sheet
-			gs.getSheets(answerKey, accessToken).then(function(sheets) {
-				if(!sheets['Control']) {
-					$scope.error = "Couldn't find control sheet";
-					return;
-				}
-
-				if(!sheets['Answers']) {
-					$scope.error = "Couldn't find answers sheet";
-					return;
-				}
-
-				answerSheet = sheets['Answers'];
-
-				gs.getRows(answerKey, sheets['Control'], accessToken).then(function(config) {
-					if(config.length == 0) {
-						$scope.error = "Couldn't determine country!";
-					}
-
-					$rootScope.country = config[0].country;
-				});
-
-				// Populate answers
-				gs.getRows(answerKey, sheets['Answers'], accessToken).then(function(answers) {
-					angular.forEach(answers, function(answer) {
-						if(!$rootScope.responses[answer.questionid]) {
-							$rootScope.responses[answer.questionid] = {};
-						}
-						
-						$rootScope.responseLinks[answer.questionid] = answer[':links'];
-
-						var response = {};
-
-						for(var col in answer) {
-							// Ignore metadata fields starting with :
-							if(col[0] != ':') {
-								response[col] = answer[col];
-							}
-						}
-
-						// Collapse multi-part responses into arrays
-						angular.forEach([ 'example' ], function(field) {
-							answer[field] = [];
-
-							for(var i = 0; i <= 10; i++) {
-								var id = field + i;
-
-								if(typeof answer[id] == 'string' && answer[id] != '' ) {
-									answer[field].push(answer[id]);
-								}
-							}
-						});
-
-						$rootScope.oldResponses[answer.questionid] = angular.copy(response);
-						$rootScope.responses[answer.questionid] = response;
-					});
-				});
-			});
-
-			// Queue up changes as answers are updated
-			var queue = {}, processQueue = {};
-
-			$scope.$on('update-answer', function(ev, qid, newValues) {
-				queue[qid] = newValues;
-			});
-
-			$interval(function() {
-				var j;
-				
-				for(var qid in queue) {
-					var values = $rootScope.responses[qid];
-
-					values = $.extend({}, {
-						response: values.response, 
-						justification: values.justification,
-						confidence: values.confidence,
-						examples: values.examples,
-					}, { 
-						questionid: qid
-					});
-
-					if(processQueue[qid]) {
-						processQueue[qid].abort();
-					}
-
-					if($rootScope.responseLinks[qid]) {
-						processQueue[qid] = gs.updateRow($rootScope.responseLinks[qid].edit, values, accessToken);
-					}
-					else {
-						processQueue[qid] = gs.insertRow(answerSheet, values, accessToken);
-					}
-
-					processQueue[qid].then(function(row) {
-						$rootScope.responseLinks[qid] = row[':links'];
-
-						var response = {};
-
-						for(var col in row) {
-							// Ignore metadata fields starting with :
-							if(col[0] != ':') {
-								response[col] = row[col];
-							}
-						}
-
-						$rootScope.oldResponses[qid] = response;
-					})['finally'](function() {
-						delete processQueue[qid];
-					});
-				}
-
-				queue = {};
-			}, 3000);
-		});
-	} ])
-	.controller('W3FSurveyController', [ 'spreadsheets', '$scope', '$rootScope', '$q', '$cookies', '$routeParams', function(gs, $scope, $rootScope, $q, $cookies, $routeParams) {	
-		var masterKey = '0AokPSYs1p9vhdEdjeUluaThWc2RqQnI0c21oN1FaYUE';
-
-		$scope.sectionOrder = [];
-		$scope.sections = {};
+		// Section order and descriptors
+		$rootScope.sectionOrder = [];
+		$rootScope.sections = {};
 
 		// Questions by Section ID
-		$scope.questions = {};
+		$rootScope.questions = {};
 
 		// Responses by question ID, as a watched scope model
-		//
-		// TODO: Load from Answer sheet
 		$rootScope.responses = {};
 
 		// Responses before any changes. Necessary because of bugs in $scope.$watchCollection 
@@ -175,8 +65,8 @@ angular.module('W3FODB', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute' ])
 					sum += number;
 				}
 
-				if(question.subquestions.length) {
-					sum += $scope.sum(question.subquestions);
+				if(question.subquestions && question.subquestions.length) {
+					sum += $rootScope.sum(question.subquestions);
 				}
 			});
 
@@ -184,95 +74,223 @@ angular.module('W3FODB', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute' ])
 		};
 
 		// Set up an initial page
-		$scope.activeSection = $cookies.section;
+		$rootScope.activeSection = $cookies.section;
 
 		// Navigate to a different section
-		$scope.navigate = function(section) {
-			$scope.activeSection = $cookies.section = section;
+		$rootScope.navigate = function(section) {
+			$rootScope.activeSection = $cookies.section = section;
 			window.scrollTo(0,0);
 		}
+
+		//
+		// Manage updating the answer sheet 
+		// 
+
+		// Queue up changes as answers are updated
+		var queue = {};
+		
+		// Keep timers for processes here, cancelling pending changes to an answer
+		// when newer changes have occured
+		var processQueue = {};
+
+		// Three-second timer
+		$interval(function() {
+			var j;
+
+			for(var qid in queue) {
+				var values = $rootScope.responses[qid];
+
+				values = $.extend({}, {
+					response: values.response, 
+					 justification: values.justification,
+					 confidence: values.confidence,
+					 examples: values.examples,
+				}, { 
+					questionid: qid
+				});
+
+				if(processQueue[qid]) {
+					processQueue[qid].abort();
+				}
+
+				if($rootScope.responseLinks[qid]) {
+					processQueue[qid] = gs.updateRow($rootScope.responseLinks[qid].edit, values, $rootScope.accessToken);
+				}
+				else {
+					processQueue[qid] = gs.insertRow(answerSheet, values, $rootScope.accessToken);
+				}
+
+				processQueue[qid].then(function(row) {
+					$rootScope.responseLinks[qid] = row[':links'];
+
+					var response = {};
+
+					for(var col in row) {
+						// Ignore metadata fields starting with :
+						if(col[0] != ':') {
+							response[col] = row[col];
+						}
+					}
+
+					$rootScope.oldResponses[qid] = response;
+				})['finally'](function() {
+					delete processQueue[qid];
+				});
+			}
+
+			queue = {};
+		}, 3000);
 
 		var populate = function(sheets) {
 			var deferred = $q.defer();
 
 			// Populate "Sections" from sections sheet
 			var populateSections = function(rows) {
-				angular.forEach(rows, function(row) {
-					$scope.sectionOrder.push(row.section);
+				angular.forEach(rows, function(section) {
+					$rootScope.sectionOrder.push(section.section);
 
 					// Default to first section
 					if(!$scope.activeSection) {
-						$scope.activeSection = row.section;
+						$scope.activeSection = section.section;
 					}
 
-					$scope.sections[row.section] = row;
-					$scope.sections[row.section].questions = [];
+					$rootScope.sections[section.section] = section;
+					$rootScope.sections[section.section].questions = [];
 				});
 			};
 
 			// Populate "Questions" from questions sheet
 			var populateQuestions = function(rows) {
-				// Track questions by ID to form parent relationships
-				var questionsById = {};
-
-				angular.forEach(rows, function(row) {
-					if(!$scope.sections[row.section]) {
+				angular.forEach(rows, function(question) {
+					if(!$scope.sections[question.section]) {
 						return;
 					}
 
 					// Gather various fields into arrays. Original fields are kept, this is just for ease of templates
 					angular.forEach([ 'option', 'guidance', 'supporting' ], function(field) {
-						row[field] = [];
+						question[field] = [];
 
 						for(var i = 0; i <= 10; i++) {
 							var id = field + i;
 
-							if(typeof row[id] == 'string' && row[id] != '' ) {
-								row[field].push({ weight: i, id: id, content: row[id] });
+							if(typeof question[id] == 'string' && question[id] != '' ) {
+								question[field].push({ weight: i, id: id, content: question[id] });
 							}
 						}
 					});
 
 					// Extract valid options from supporting information fields
-					angular.forEach(row.supporting, function(option) {
+					angular.forEach(question.supporting, function(option) {
 						var matches = option.content.match(/^\s*(?:(\d+(?:\s*,\s*\d+)*)\s*;)?\s*(.+)\s*$/i);
 
 						option.values = matches[1] && matches[1].split(/\s*,\s*/);
 						option.content = matches[2];
 					});
 
-					// Make sure responses have somewhere to go
-					if(!$rootScope.responses[row.questionid]) {
-						$rootScope.responses[row.questionid] = {};
-						$rootScope.oldResponses[row.questionid] = {};
-					}
+					$rootScope.responses[question.questionid] = {};
 
-					// Watch responses to add any changes to the save queue
-					// BUG: oldValue and newValue are the same in this call from $watchCollection -
-					// See: https://github.com/angular/angular.js/issues/2621. 
-					$rootScope.$watchCollection("responses['" + row.questionid + "']", function(oldValue, newValue) {
-						if(!angular.equals(newValue, $scope.oldResponses[row.questionid])) {
-							$rootScope.$broadcast('update-answer', row.questionid, newValue);
-						}
+					// Update progress bar as responses are given
+					$rootScope.$watchCollection('responses["' + question.questionid + '"]', function(newValue) {
+						$rootScope.$broadcast('response-updated');
 					});
 
 					// Nest subquestions here
-					row.subquestions = [];
+					question.subquestions = [];
 
 					// Save a reference to the question by ID
-					$scope.questions[row.questionid] = row;
+					$rootScope.questions[question.questionid] = question;
 
 					// Child questions, assume parent has already been registered.
-					if(row.parentid) {
-						$scope.questions[row.parentid].subquestions.push(row);
+					if(question.parentid) {
+						$rootScope.questions[question.parentid].subquestions.push(question);
 					}
-					// Top-level question
+					// Top-level question in this section
 					else {
-						$scope.sections[row.section].questions.push(row);
+						$rootScope.sections[question.section].questions.push(question);
 					}
 				});
+			}
 
-				deferred.resolve();
+			// Load answer sheet and populate responses model
+			var loadAnswers = function() {
+				// Try to get answer sheet
+				gs.getSheets(answerKey, $rootScope.accessToken).then(function(sheets) {
+					if(!sheets['Control']) {
+						$scope.error = "Couldn't find control sheet";
+						return;
+					}
+
+					if(!sheets['Answers']) {
+						$scope.error = "Couldn't find answers sheet";
+						return;
+					}
+
+					// Get any answer metadata from control sheet
+					gs.getRows(answerKey, sheets['Control'], $rootScope.accessToken).then(function(config) {
+						if(config.length == 0) {
+							$scope.error = "Couldn't determine country!";
+						}
+
+						$rootScope.country = config[0].country;
+					});
+
+					// Populate answers. This can be done in parralel with control data load
+					// since the data sets are distinct
+					gs.getRows(answerKey, sheets['Answers'], $rootScope.accessToken).then(function(answers) {
+						angular.forEach(answers, function(answer) {
+							if(!$rootScope.questions[answer.questionid]) {
+								console.log("Answer with qid=" + answer.questionid + " does not correspond to any survey question");
+								return;
+							}
+
+							$rootScope.responseLinks[answer.questionid] = answer[':links'];
+
+							var response = $rootScope.responses[answer.questionid] 
+
+							// Copy all response properties from sheet into row
+							for(var col in answer) {
+								// Ignore metadata fields starting with :
+								if(col[0] != ':') {
+									response[col] = answer[col];
+								}
+							}
+
+							// Collapse multi-part responses into arrays
+							angular.forEach([ 'example' ], function(field) {
+								answer[field] = [];
+
+								for(var i = 0; i <= 10; i++) {
+									var id = field + i;
+
+									if(typeof answer[id] == 'string' && answer[id] != '' ) {
+										answer[field].push(answer[id]);
+									}
+								}
+							});
+
+							$rootScope.oldResponses[answer.questionid] = _.clone(response);
+						});
+
+						// Only now that the answer sheet has been loaded
+						// do we watch for changes to the responses that might
+						// come from the user.
+						//
+						// Watch responses to add any changes to the save queue
+						//
+						// BUG: oldValue and newValue are the same in this call from $watchCollection -
+						// See: https://github.com/angular/angular.js/issues/2621. 
+						_.each(_.keys($rootScope.questions), function(qid) {
+							$rootScope.$watchCollection("responses['" + qid + "']", function(oldValue, newValue) {
+								if(!angular.equals(newValue, $rootScope.oldResponses[qid])) {
+									queue[qid] = newValue;
+									$rootScope.oldResponses[qid] = newValue;
+								}
+							});
+						});
+
+						deferred.resolve();
+					});
+				});
 			}
 
 			gs.getRows(masterKey, sheets['Sections'], $rootScope.accessToken).then(function(sections) {
@@ -280,6 +298,13 @@ angular.module('W3FODB', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute' ])
 
 				gs.getRows(masterKey, sheets['Questions'], $rootScope.accessToken).then(function(questions) {
 					populateQuestions(questions);
+
+					if(answerKey) {
+						loadAnswers();
+					}
+					else {
+						deferred.resolve();
+					}
 				});
 			});
 
@@ -292,7 +317,7 @@ angular.module('W3FODB', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute' ])
 				return;
 			}
 
-			if(!authResult.status.signed_in || $scope.accessToken) {
+			if(!authResult.status.signed_in || $rootScope.accessToken) {
 				return;
 			}
 
@@ -315,11 +340,13 @@ angular.module('W3FODB', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute' ])
 					$scope.loading = false;
 					$scope.loaded = true;
 
-					$scope.$broadcast('sections-loaded');
+					$rootScope.$broadcast('sections-loaded');
 				});
 			});
 		};
 	} ])
+
+	// Create a rail exactly the size of the sections menu
 	.directive('withRail', [ '$timeout', function($timeout) {
 		return {
 			link: function($scope, element, attrs) {
@@ -337,6 +364,30 @@ angular.module('W3FODB', [ 'GoogleSpreadsheets', 'ngCookies', 'ngRoute' ])
 			}
 		}
 	} ])
+
+	// Create a rail exactly the size of the sections menu
+	.directive('updateOnResponse', [ '$timeout', function($timeout) {
+		return {
+			link: function($scope, element, attrs) {
+				$scope.$on('response-updated', function() {
+					$scope.sectionAnswers = [];
+					$scope.sectionQuestions = _.filter($scope.questions, function(q) { 
+						if(q.section == $scope.section) {
+							if(!_.isEmpty($scope.responses[q.questionid].response)) {
+								$scope.sectionAnswers.push($scope.responses[q.questionid]);
+							}
+
+							return true;
+						}
+
+						return false;
+					});
+				});
+			}
+		}
+	} ])
+
+	// Allow for insert/update/delete operations on a list of text inputs
 	.directive('flexibleList', [ function() {
 		return {
 			replace: true,
